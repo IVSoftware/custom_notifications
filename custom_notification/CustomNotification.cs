2f2ab4e4c8e4ea915f52cb76ba6fac62927ba6f9
Tom Gregor <tgregor@ivsoftware.com>
2022-07-02 11:04:12 -0600

    Add project files.

---
+++ custom_notification/CustomNotification.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace custom_notification
+{
+    public partial class CustomNotification : UserControl
+    {
+        public CustomNotification(string message)
+        {
+            InitializeComponent();
+            labelMessage.Text = message;
+            buttonDelete.Click += (sender, e) =>
+            {
+                Dispose();
+            };
+        }
+        public new void Dispose()
+        {
+            CurrentNotifications.Remove(this);
+            RecalcLocations();
+            base.Dispose();
+        }
+
+        private static void RecalcLocations()
+        {
+            for (int i = 0; i < CurrentNotifications.Count; i++)
+            {
+                var notification = CurrentNotifications[i];
+                notification.Location = new Point(0, (notification.Height + 10) * i);
+            }
+        }
+
+        static List<CustomNotification> CurrentNotifications { get; } = new List<CustomNotification>();
+        public static void Show(IWin32Window owner, string message)
+        {
+            var notification = new CustomNotification(message);
+            if (owner is Control control)
+            {
+                control.Controls.Add(notification);
+                notification.Width = control.ClientRectangle.Width;
+                CurrentNotifications.Add(notification);
+                RecalcLocations();
+            }
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            _mouseDownLocation = Location;
+            _mouseDownX = PointToScreen(e.Location).X;            
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            var delta = Math.Max(0, PointToScreen(e.Location).X - _mouseDownX);
+            if(delta > (3 * Width) / 4)
+            {
+                Dispose();
+            }
+            else
+            {
+                Location = _mouseDownLocation;
+            }
+        }
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            buttonDelete.Visible = true;
+            labelMessage.ForeColor = Color.LightYellow;
+        }
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            if(null == GetChildAtPoint(PointToClient(MousePosition)))
+            {
+                buttonDelete.Visible = false;
+                labelMessage.ForeColor = Color.White;
+            }
+        }
+
+        SemaphoreSlim _ssReenter = new SemaphoreSlim(1, 1);
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if(_ssReenter.Wait(0))
+            {
+                try
+                {
+                    if(MouseButtons == MouseButtons.Left)
+                    {
+                        var delta = Math.Max(0, PointToScreen(e.Location).X - _mouseDownX);
+                        Location = new Point(delta, Location.Y);
+                    }
+                }
+                finally
+                {
+                    _ssReenter.Release();
+                }
+            }
+        }
+        private Point _mouseDownLocation;
+        private int _mouseDownX;
+    }
+}
---
+++ custom_notification/CustomNotificationContainer.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace custom_notification
+{
+    partial class CustomNotificationContainer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxDocked = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBoxDocked
+            // 
+            this.checkBoxDocked.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxDocked.AutoSize = true;
+            this.checkBoxDocked.Location = new System.Drawing.Point(12, 703);
+            this.checkBoxDocked.Name = "checkBoxDocked";
+            this.checkBoxDocked.Size = new System.Drawing.Size(99, 29);
+            this.checkBoxDocked.TabIndex = 0;
+            this.checkBoxDocked.Text = "Docked";
+            this.checkBoxDocked.UseVisualStyleBackColor = true;
+            // 
+            // CustomNotificationContainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 744);
+            this.Controls.Add(this.checkBoxDocked);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CustomNotificationContainer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Container";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxDocked;
+    }
+}
